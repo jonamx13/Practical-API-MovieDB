00d929293dceaa0299b034509b68fb8f4e02ad4a
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-08-11 14:22:47 -0600

    DOM for categories

--- repages/main.js
+++ repages/main.js
@@ -4,7 +4,7 @@ async function getTrendingMoviesPreview() {
 
     const movies = data.results;
     movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
@@ -21,4 +21,29 @@ async function getTrendingMoviesPreview() {
     });
 }
 
-getTrendingMoviesPreview()
+async function getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+        categoryTitle.classList.add('category-title');
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+        
+    });
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
